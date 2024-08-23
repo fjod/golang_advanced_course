e92844df94c7310f3bc50e3fbcd88f9a53fcba2d
Michael <fourjustoneday@yandex.ru>
2024-08-23 16:12:15 +0300

    iter7

--- cmd/agent/Sender.go
+++ cmd/agent/Sender.go
@@ -7,31 +7,23 @@ import (
 	"github.com/fjod/golang_advanced_course/internal"
 	data "github.com/fjod/golang_advanced_course/internal/Data"
 	"net/http"
-	"sync"
 	"time"
 )
 
-var sender = &sync.Mutex{}
-
 func SendMetrics(server string, reportInterval int, pollInterval int) {
-	chg10s := make(chan data.Gauge)
-	chc10s := make(chan data.Counter)
+	chg10s := make(chan data.Gauge, 1)
+	chc10s := make(chan data.Counter, 1)
 	sleepDur := time.Duration(100) * time.Millisecond
 	go internal.CollectMetrics(pollInterval, reportInterval, chg10s, chc10s)
 	for {
 		select {
 		case g := <-chg10s:
-
-			sender.Lock()
 			fmt.Println("отправка gauge ", g.GetName(), " ", g.GetValue())
 			send(g, server)
-			sender.Unlock()
 
 		case c := <-chc10s:
-			sender.Lock()
 			fmt.Println("отправка counter ", c.GetName(), " ", c.GetValue())
 			send(c, server)
-			sender.Unlock()
 
 		default:
 			time.Sleep(sleepDur)
