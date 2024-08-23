1c5790b8124d4c07d81a1e55de30af793de3feea
Michael <fourjustoneday@yandex.ru>
2024-08-23 14:30:34 +0300

    iter7

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/fjod/golang_advanced_course/internal"
-	H "github.com/fjod/golang_advanced_course/internal/handlers"
+	H "github.com/fjod/golang_advanced_course/internal/Handlers"
 	MW "github.com/fjod/golang_advanced_course/internal/middlewares"
 	"github.com/gin-gonic/gin"
 	"go.uber.org/zap"
