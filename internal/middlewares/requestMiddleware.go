7c624397639e0663dca7f44148cff5270dd5fe99
Michael <fourjustoneday@yandex.ru>
2024-08-23 15:29:44 +0300

    iter7

--- internal/middlewares/requestMiddleware.go
+++ internal/middlewares/requestMiddleware.go
@@ -1,6 +1,7 @@
 package middlewares
 
 import (
+	data "github.com/fjod/golang_advanced_course/internal/Data"
 	"github.com/gin-gonic/gin"
 	"go.uber.org/zap"
 	"time"
@@ -9,6 +10,23 @@ import (
 func RequestLogger(sugar zap.SugaredLogger) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		t := time.Now()
+
+		var d data.Metrics
+		err := c.ShouldBindJSON(&d)
+		if err == nil {
+			if d.MType == "counter" {
+				sugar.Infoln(
+					"data", d,
+					"value", *d.Delta,
+				)
+			}
+			if d.MType == "gauge" {
+				sugar.Infoln(
+					"data", d,
+					"value", *d.Value,
+				)
+			}
+		}
 
 		c.Next()
 
