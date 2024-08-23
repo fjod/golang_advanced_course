7ede3dab6d4663233bf92450a986c30aabd7d116
Michael <fourjustoneday@yandex.ru>
2024-08-23 16:04:40 +0300

    iter7

--- cmd/agent/Sender.go
+++ cmd/agent/Sender.go
@@ -7,8 +7,11 @@ import (
 	"github.com/fjod/golang_advanced_course/internal"
 	data "github.com/fjod/golang_advanced_course/internal/Data"
 	"net/http"
+	"sync"
 	"time"
 )
+
+var sender = &sync.Mutex{}
 
 func SendMetrics(server string, reportInterval int, pollInterval int) {
 	chg10s := make(chan data.Gauge)
@@ -19,13 +22,16 @@ func SendMetrics(server string, reportInterval int, pollInterval int) {
 		select {
 		case g := <-chg10s:
 
+			sender.Lock()
 			fmt.Println("отправка gauge ", g.GetName(), " ", g.GetValue())
 			send(g, server)
+			sender.Unlock()
 
 		case c := <-chc10s:
-
+			sender.Lock()
 			fmt.Println("отправка counter ", c.GetName(), " ", c.GetValue())
 			send(c, server)
+			sender.Unlock()
 
 		default:
 			time.Sleep(sleepDur)
@@ -34,10 +40,9 @@ func SendMetrics(server string, reportInterval int, pollInterval int) {
 }
 
 func send(m data.IMetric, server string) {
-	fmt.Println("пробуем что-то отправить")
 	s := fmt.Sprintf("http://%v/update/", server)
 	var j = m.ToJSON()
-	fmt.Println(j)
+	fmt.Printf("пробуем что-то отправить %v\n", j)
 	jsonData, err := json.Marshal(j)
 	if err != nil {
 		fmt.Println(err)
@@ -46,8 +51,6 @@ func send(m data.IMetric, server string) {
 	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Println(resp.Status)
-	fmt.Println("пробуем закрыть body")
 	err = resp.Body.Close()
 	if err != nil {
 		fmt.Println(err)
